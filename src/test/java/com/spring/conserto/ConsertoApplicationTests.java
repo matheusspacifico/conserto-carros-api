4c30d523bb3727e9574308c35e4f00cf33036651
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-25 20:36:17 -0300

    refactor: change application name from carros to conserto

--- src/test/java/com/spring/conserto/ConsertoApplicationTests.java
+++ src/test/java/com/spring/conserto/ConsertoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarrosApplicationTests {
+class ConsertoApplicationTests {
 
 	@Test
 	void contextLoads() {
