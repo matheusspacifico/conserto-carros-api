8772bce50c93857cd1bbb38a133d82418b86e479
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-25 21:02:12 -0300

    feat(dto): add jakarta validation

--- src/main/java/com/spring/conserto/model/conserto/ConsertoDTO.java
+++ src/main/java/com/spring/conserto/model/conserto/ConsertoDTO.java
@@ -1,14 +1,29 @@
 package com.spring.conserto.model.conserto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.spring.conserto.model.mecanico.MecanicoDTO;
 import com.spring.conserto.model.veiculo.VeiculoDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record ConsertoDTO(
+
+        @NotNull
+        @JsonFormat(pattern = "dd-MM-yyyy")
         LocalDate dataEntradaOficina,
+
+        @NotNull
+        @JsonFormat(pattern = "dd-MM-yyyy")
         LocalDate dataSaidaOficina,
+
+        @NotNull
+        @Valid
         MecanicoDTO mecanicoResponsavel,
+
+        @NotNull
+        @Valid
         VeiculoDTO veiculoResponsavel
 ) {
 }
--- src/main/java/com/spring/conserto/model/mecanico/MecanicoDTO.java
+++ src/main/java/com/spring/conserto/model/mecanico/MecanicoDTO.java
@@ -1,7 +1,12 @@
 package com.spring.conserto.model.mecanico;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record MecanicoDTO(
+
+        @NotBlank
         String nome,
+
         int anosExperiencia
 ) {
 }
