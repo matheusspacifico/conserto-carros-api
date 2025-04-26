164ea92c091ed0f0bbdddd1523bdfee5f2ce133f
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-25 21:03:36 -0300

    feat: add new getting map that returns less data

--- src/main/java/com/spring/conserto/controller/ConsertoController.java
+++ src/main/java/com/spring/conserto/controller/ConsertoController.java
@@ -2,13 +2,18 @@ package com.spring.conserto.controller;
 
 import com.spring.conserto.model.conserto.Conserto;
 import com.spring.conserto.model.conserto.ConsertoDTO;
+import com.spring.conserto.model.conserto.ConsertoListagemDTO;
 import com.spring.conserto.repository.ConsertoRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/consertos")
@@ -19,9 +24,17 @@ public class ConsertoController {
 
     @PostMapping
     @Transactional
-    public void cadastrarConserto(@RequestBody ConsertoDTO consertoDTO) {
+    public void cadastrarConserto(@RequestBody @Valid ConsertoDTO consertoDTO) {
         repository.save(new Conserto(consertoDTO));
     }
 
+    @GetMapping
+    public Page<Conserto> listarTodosOsConsertos(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
 
+    @GetMapping("algunsdados")
+    public List<ConsertoListagemDTO> listarAlgunsDados() {
+        return repository.findAll().stream().map(ConsertoListagemDTO::new).toList();
+    }
 }
---
+++ src/main/java/com/spring/conserto/model/conserto/ConsertoListagemDTO.java
@@ -0,0 +1,22 @@
+package com.spring.conserto.model.conserto;
+
+import com.spring.conserto.model.veiculo.Marca;
+import java.time.LocalDate;
+
+public record ConsertoListagemDTO(
+        LocalDate dataEntradaOficina,
+        LocalDate dataSaidaOficina,
+        String nomeMecanico,
+        Marca marcaVeiculo,
+        String modeloVeiculo
+) {
+        public ConsertoListagemDTO(Conserto conserto) {
+                this(
+                        conserto.getDataEntradaOficina(),
+                        conserto.getDataSaidaOficina(),
+                        conserto.getMecanicoResponsavel().getNome(),
+                        conserto.getVeiculoResponsavel().getMarca(),
+                        conserto.getVeiculoResponsavel().getModelo()
+                );
+        }
+}
