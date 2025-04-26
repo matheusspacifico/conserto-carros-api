c001aacb051d7ae51e370f6ed59b79a14b8a8612
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-25 23:30:51 -0300

    fix(ConsertoAtualizacaoDTO): implement json date format

--- src/main/java/com/spring/conserto/model/conserto/ConsertoAtualizacaoDTO.java
+++ src/main/java/com/spring/conserto/model/conserto/ConsertoAtualizacaoDTO.java
@@ -1,5 +1,6 @@
 package com.spring.conserto.model.conserto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
@@ -9,9 +10,17 @@ public record ConsertoAtualizacaoDTO(
         @NotNull
         Long id,
 
+        @JsonFormat(pattern = "dd-MM-yyyy")
         LocalDate dataSaidaOficina,
         String nomeMecanico,
         int anosExperiencia
 ) {
-
+        public ConsertoAtualizacaoDTO(Conserto conserto) {
+                this(
+                        conserto.getId(),
+                        conserto.getDataSaidaOficina(),
+                        conserto.getMecanicoResponsavel().getNome(),
+                        conserto.getMecanicoResponsavel().getAnosExperiencia()
+                );
+        }
 }
