296a132da4361fe6baf71badd666e972d32127d8
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-25 22:53:05 -0300

    refactor(controller): update return type to ResponseEntity

--- src/main/java/com/spring/conserto/controller/ConsertoController.java
+++ src/main/java/com/spring/conserto/controller/ConsertoController.java
@@ -1,19 +1,14 @@
 package com.spring.conserto.controller;
 
-import com.spring.conserto.model.conserto.Conserto;
-import com.spring.conserto.model.conserto.ConsertoAtualizacaoDTO;
-import com.spring.conserto.model.conserto.ConsertoDTO;
-import com.spring.conserto.model.conserto.ConsertoListagemDTO;
+import com.spring.conserto.model.conserto.*;
 import com.spring.conserto.repository.ConsertoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,27 +22,31 @@ public class ConsertoController {
 
     @PostMapping
     @Transactional
-    public void cadastrarConserto(@RequestBody @Valid ConsertoDTO consertoDTO) {
-        repository.save(new Conserto(consertoDTO));
+    public ResponseEntity cadastrarConserto(@RequestBody @Valid ConsertoDTO consertoDTO, UriComponentsBuilder uriBuilder) {
+        Conserto conserto = new Conserto(consertoDTO);
+        repository.save(conserto);
+
+        var uri = uriBuilder.path("/consertos/{id}").buildAndExpand(conserto.getId()).toUri();
+        return ResponseEntity.created(uri).body(new ConsertoRetornoDTO(conserto));
     }
 
     @GetMapping
-    public Page<Conserto> listarTodosOsConsertos(Pageable pageable) {
-        return repository.findAll(pageable);
+    public ResponseEntity listarTodosOsConsertos(Pageable pageable) {
+        return ResponseEntity.ok(repository.findAll(pageable));
     }
 
     @GetMapping("algunsdados")
-    public List<ConsertoListagemDTO> listarAlgunsDados() {
-        return repository.findAll().stream().map(ConsertoListagemDTO::new).toList();
+    public ResponseEntity listarAlgunsDados() {
+        return ResponseEntity.ok(repository.findAllByAtivoTrue().stream().map(ConsertoListagemDTO::new).toList());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Conserto> listarConsertoPorId(@PathVariable Long id) {
+    public ResponseEntity listarConsertoPorId(@PathVariable Long id) {
         Optional<Conserto> consertoOptional = repository.findById(id);
 
         if (consertoOptional.isPresent()) {
             Conserto conserto = consertoOptional.get();
-            return ResponseEntity.ok(conserto);
+            return ResponseEntity.ok(new ConsertoRetornoDTO(conserto));
         }
 
         return ResponseEntity.notFound().build();
@@ -55,15 +54,19 @@ public class ConsertoController {
 
     @PutMapping
     @Transactional
-    public void atualizarConserto(@RequestBody @Valid ConsertoAtualizacaoDTO consertoAtualizacaoDTO) {
+    public ResponseEntity atualizarConserto(@RequestBody @Valid ConsertoAtualizacaoDTO consertoAtualizacaoDTO) {
         Conserto conserto = repository.getReferenceById(consertoAtualizacaoDTO.id());
         conserto.atualizarInformacoes(consertoAtualizacaoDTO);
+
+        return ResponseEntity.ok(new ConsertoRetornoDTO(conserto));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluirConserto(@PathVariable Long id) {
+    public ResponseEntity excluirConserto(@PathVariable Long id) {
         Conserto conserto = repository.getReferenceById(id);
         conserto.cancelarConserto();
+
+        return ResponseEntity.noContent().build();
     }
 }
