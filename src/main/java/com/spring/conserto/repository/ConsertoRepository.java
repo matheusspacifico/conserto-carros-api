2e9c0cd86018effcefa618f3a26c05d29a2d1985
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-25 22:52:33 -0300

    feat(repository): implement get by active conserto method

--- src/main/java/com/spring/conserto/repository/ConsertoRepository.java
+++ src/main/java/com/spring/conserto/repository/ConsertoRepository.java
@@ -3,5 +3,8 @@ package com.spring.conserto.repository;
 import com.spring.conserto.model.conserto.Conserto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConsertoRepository extends JpaRepository<Conserto, Long> {
+    List<Conserto> findAllByAtivoTrue();
 }
