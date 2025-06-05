f4c1daeb7c0bb994d3b509ed7be007c5a9200eb9
matheusspacifico <matheuspacifico44@gmail.com>
2025-06-05 14:07:46 -0300

    feat: add authentication repository and service

---
+++ src/main/java/com/spring/conserto/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.spring.conserto.repository;
+
+import com.spring.conserto.model.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
