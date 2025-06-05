4f8bd1b6e06eadf11bfa17cdd431348a1b433875
matheusspacifico <matheuspacifico44@gmail.com>
2025-06-05 14:51:06 -0300

    feat: add authentication manager security configuration

--- src/main/java/com/spring/conserto/util/security/SecurityConfiguration.java
+++ src/main/java/com/spring/conserto/util/security/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.spring.conserto.util.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,5 +19,10 @@ public class SecurityConfiguration {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
