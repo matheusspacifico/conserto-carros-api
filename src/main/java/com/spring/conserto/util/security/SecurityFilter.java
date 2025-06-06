35d451ae074ace76ad4cb7bb9abd2a0c112f6876
matheusspacifico <matheuspacifico44@gmail.com>
2025-06-06 18:42:27 -0300

    refactor: add non-null annotation to security filter

--- src/main/java/com/spring/conserto/util/security/SecurityFilter.java
+++ src/main/java/com/spring/conserto/util/security/SecurityFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -23,7 +24,10 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
 
         var authorizationHeader = request.getHeader("Authorization");
 
